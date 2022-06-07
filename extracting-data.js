dc218e1f914386459b18513e1936f47bd620293f
marcuspousette <marcuspousette@gmail.com>
2022-06-07 17:09:08 +0200

    init

---
+++ extracting-data.js
@@ -0,0 +1,13 @@
+const employee = {
+  name: "ANE01",
+  email: "Anna@example.com",
+  phone: "01123456789",
+  isMale: true,
+};
+
+const name = employee.name;
+const email = employee.email;
+const phone = employee.phone;
+
+const oldEmployees = ["Mike", "Nabil", "Cubby", "Mendes"];
+const newEmployees = ["Marcus", "Rasmus", "Matt"];
