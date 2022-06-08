cb9e9a9b74b8c9dfd1a9a166cdad965149efd61e
marcuspousette <marcuspousette@gmail.com>
2022-06-08 08:52:18 +0200

    added functions to create new arrays

---
+++ array.js
@@ -0,0 +1,11 @@
+const oldEmployees = ["Mike", "Nabil", "Cubby", "Mendes"];
+const newEmployees = ["Marcus", "Rasmus", "Matt"];
+const allEmployees = [];
+
+oldEmployees.forEach((employee) => {
+  allEmployees.push(employee);
+});
+
+newEmployees.forEach((employee) => {
+  allEmployees.push(employee);
+});
