e43bc510f796bae8d2f2308c3c6d444b9089ac69
Marcus Pousette <marcuspousette@gmail.com>
2022-06-09 08:55:16 +0200

    create

---
+++ create.js
@@ -0,0 +1,10 @@
+const employee = {
+  name: 'ANE01',
+  email: 'Anna@example.com',
+  phone: '01123456789',
+  isMale: true,
+};
+
+const name = employee.name;
+const email = employee.email;
+const phone = employee.phone;
